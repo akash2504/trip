75e9841aefffc43699d0901c525360dcecfe68f6
akash <akash.bansal2504@gmail.com>
2014-08-08 19:33:41 +0530

    Hello World

--- index.php
+++ index.php
@@ -1,53 +1,15 @@
-
-<titled>
-    <meta charset="utf-8">
-    <title>My Website</title>
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- <title>Bootstrap 101 Template</title> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
-    <script src="js/respond.js"></script>
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="css/mystyles.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <script src="js/bootstrap.js"></script>
     <script src="js/myscript.js"></script>
-</head>
-<body>
-<div id="tab" class="color black">
-     <div class="container">
-          <nav class="row">
-              <ul class="nav navbar-nav">
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">Photo Gallery</a></li>
-                  <li><a href="#">Contact</a></li> 
-             </ul> 
-         </nav>
-     </div>
-</div>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-<div class="container"
-<ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
-
-<div class="carousel-inner">
-    <div class="item active">
-      <img src="img/1.jpg" alt="">
-      <div class="carousel-caption">
-        Hello world!!
-      </div>
-    </div>
-</div>
-</div>
-
-
-<div id="pricing" color="color black">
-     <div class="container">
-     </div>
-</div>
-<?php
-include "./php/footer.php";
-?>
-</body>
-</html>>My Website</title>
+  </body>
+</html>
