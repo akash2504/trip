20cf26f03d89c6d27597603450f5eb652501c201
akash <akash.bansal2504@gmail.com>
2014-08-08 19:59:46 +0530

    added footer

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer class="row">
+         <p><small>This is a website where you can plan your travel ijn nepal</small></p>
+         <p><small>Spend your Holiday in a enthusiastic way</small></p>
+    </footer>
--- index.php
+++ index.php
@@ -7,9 +7,19 @@
     <link href="css/mystyles.css" rel="stylesheet" media="screen">
   </head>
   <body>
+  <div class="container">
+  <div class="row">
     <h1>Hello, world!</h1>
-
+</div>
+ <div class="row">
+ </div>
     <script src="js/bootstrap.js"></script>
     <script src="js/myscript.js"></script>
+    <?php
+      include ('footer.php');
+      ?>
+     </div>
+
   </body>
+
 </html>
